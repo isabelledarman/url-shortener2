36b248d6ccc31c0f61c477c1d0479d0e7e5d0fb5
isabelledarman <135927120+isabelledarman@users.noreply.github.com>
2024-05-01 12:25:06 -0400

    New files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from './Home'
+import GoLink from './GoLink'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+            <Route exact path = "/" Component={Home}/>
+            <Route exact path = "/l/:code" Component={GoLink}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
